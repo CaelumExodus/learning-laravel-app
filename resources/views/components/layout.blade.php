b358fadd8e3251b93af3d7a2b4b6c223c8d55fc9
Caelum Exodus <tea418pot@gmail.com>
2023-02-14 12:44:53 +0100

    created create page with routing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -58,10 +58,10 @@
     bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center
   "
 >
-  <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+  <p class="ml-2">Copyright &copy; 2022, All Rights reserveddd</p>
 
   <a
-    href="create.html"
+    href="/listings/create"
     class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
   >Post Job</a
   >
