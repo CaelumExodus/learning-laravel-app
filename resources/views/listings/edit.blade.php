9ff84fe265afa1075cbb887bfea10b607309cdb9
Caelum Exodus <tea418pot@gmail.com>
2023-02-21 20:44:22 +0100

    Edit existing gig

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -2,13 +2,14 @@
   <x-card class="p10 max-w-lg mx-auto mt-24">
     <header class="text-center">
       <h2 class="text-2xl font-bold uppercase mb-1">
-        Create a Gig
+        Edit a Gig
       </h2>
-      <p class="mb-4">Post a gig to find a developer</p>
+      <p class="mb-4">Edit {{$listing->title}}</p>
     </header>
 
-    <form method="POST" action="/listings" enctype="multipart/form-data">
+    <form method="POST" action="/listings/{{$listing->id}}" enctype="multipart/form-data">
       @csrf
+      @method('PUT')
       <div class="mb-6">
         <label
           for="company"
@@ -19,7 +20,7 @@
           type="text"
           class="border border-gray-200 rounded p-2 w-full"
           name="company"
-          value="{{old('company')}}"
+          value="{{$listing->company}}"
         />
 
         @error('company')
@@ -38,7 +39,7 @@
           class="border border-gray-200 rounded p-2 w-full"
           name="title"
           placeholder="Example: Senior Laravel Developer"
-          value="{{old('title')}}"
+          value="{{$listing->title}}"
         />
 
         @error('title')
@@ -59,7 +60,7 @@
           class="border border-gray-200 rounded p-2 w-full"
           name="location"
           placeholder="Example: Remote, Boston MA, etc"
-          value="{{old('location')}}"
+          value="{{$listing->location}}"
         />
 
         @error('location')
@@ -77,7 +78,7 @@
           type="text"
           class="border border-gray-200 rounded p-2 w-full"
           name="email"
-          value="{{old('email')}}"
+          value="{{$listing->email}}"
         />
 
         @error('email')
@@ -98,7 +99,7 @@
           type="text"
           class="border border-gray-200 rounded p-2 w-full"
           name="website"
-          value="{{old('website')}}"
+          value="{{$listing->website}}"
         />
 
         @error('website')
@@ -117,7 +118,7 @@
           class="border border-gray-200 rounded p-2 w-full"
           name="tags"
           placeholder="Example: Laravel, Backend, Postgres, etc"
-          value="{{old('tags')}}"
+          value="{{$listing->tags}}"
         />
 
         @error('tags')
@@ -127,22 +128,28 @@
         @enderror
       </div>
 
-            <div class="mb-6">
-              <label for="logo" class="inline-block text-lg mb-2">
-                Company Logo
-              </label>
-              <input
-                type="file"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="logo"
-              />
+      <div class="mb-6">
+        <label for="logo" class="inline-block text-lg mb-2">
+          Company Logo
+        </label>
+        <input
+          type="file"
+          class="border border-gray-200 rounded p-2 w-full"
+          name="logo"
+        />
 
-              @error('logo')
-              <p class="text-red-500 text-xs mt-1">
-                {{ $message }}
-              </p>
-              @enderror
-            </div>
+        <img
+          class="w-48 mr-6 mb-6"
+          src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}"
+          alt=""
+        />
+
+        @error('logo')
+        <p class="text-red-500 text-xs mt-1">
+          {{ $message }}
+        </p>
+        @enderror
+      </div>
 
       <div class="mb-6">
         <label
@@ -156,9 +163,7 @@
           name="description"
           rows="10"
           placeholder="Include tasks, requirements, salary, etc"
-        >
-          {{old('description')}}
-
+        >{{$listing->company}}
         </textarea>
 
         @error('description')
@@ -172,7 +177,7 @@
         <button
           class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
         >
-          Create Gig
+          Update Gig
         </button>
 
         <a href="/" class="text-black ml-4"> Back </a>
